7672c951f2e8501b163f0f61a1f7a5ebad1959a1
rsilvagit <122787307+rsilvagit@users.noreply.github.com>
2023-04-12 00:40:58 -0300

    reposicionamento proj

---
+++ MedicalTech/MedicalTech/Models/Medico.cs
@@ -0,0 +1,12 @@
+﻿namespace MedicalTech.Models
+{
+    public class Medico:Pesssoa
+    {
+       public string InstEnsinoForm { get; set; }
+       public string? Crm { get; set; }
+       public string? EspClinica { get; set; }
+       public bool StatusSistema { get; set; }
+       public int TotalAtendimentos { get; set; }
+
+    }
+}
