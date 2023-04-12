eafd352fc63250dd10ca04f8b8a48e80a2add801
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-04-12 01:11:01 -0300

    conection string

--- MedicalTech/MedicalTech/Program.cs
+++ MedicalTech/MedicalTech/Program.cs
@@ -1,3 +1,6 @@
+using MedicalTech.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string connectionString = "Server=DESKTOP-9HO92VC\\SQLEXPRESS;Database=labmedicinebd;Trusted_Connection=True;TrustServerCertificate=True;";
+builder.Services.AddDbContext<MedicalTechContext>(o => o.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
