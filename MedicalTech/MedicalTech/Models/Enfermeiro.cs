27de3828b944249f8da4884a82ab843e4f9c35c7
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-04-12 00:59:29 -0300

    enfermeiros class

---
+++ MedicalTech/MedicalTech/Models/Enfermeiro.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MedicalTech.Models
+{
+    [Table("Enfermeiro")]
+    public class Enfermeiro:Pesssoa
+    {
+        public string? InstEnsFormacao { get; set; }
+        public string? Cofen { get; set; }
+    }
+}
